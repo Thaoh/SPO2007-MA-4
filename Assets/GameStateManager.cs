647f156a348d9e83e2051b25f0fca3f0b41bc521
dtfe <jonas@jerner.no>
2025-03-25 14:52:12 +0100

    UGS implemented

---
+++ Assets/GameStateManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.CloudSave;
+using Unity.Services.CloudSave.Models;
+using Unity.Services.CloudSave.Models.Data.Player;
+using SaveOptions = Unity.Services.CloudSave.Models.Data.Player.SaveOptions;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.Events;
+
+public class GameStateManager : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField textField, numberField;
+    [SerializeField] private TMP_Text nameText, scoreText;
+
+    public UnityEvent OnDataSaved = new UnityEvent();
+    public UnityEvent OnDataLoaded = new UnityEvent();
+    
+
+    public async void SaveData()
+    {
+        if (textField.text != "" && numberField.text != "")
+        {
+            var playerData = new Dictionary<string, object>{
+                {"Name", textField.text},
+                {"Score", numberField.text}
+            };
+            await CloudSaveService.Instance.Data.Player.SaveAsync(playerData);
+            Debug.Log($"Saved data {string.Join(',', playerData)}");
+        }
+        else
+        {
+            return;
+        }
+        OnDataSaved.Invoke();
+    }
+
+    public async void LoadData()
+    {
+        var playerData = await CloudSaveService.Instance.Data.Player.LoadAsync(new HashSet<string> {
+            "Name", "Score"
+        });
+
+        if (playerData.TryGetValue("Name", out var firstKey)) {
+            nameText.text = firstKey.Value.GetAs<string>();
+        }
+
+        if (playerData.TryGetValue("Score", out var secondKey)) {
+            scoreText.text = secondKey.Value.GetAs<string>();
+        }
+        OnDataLoaded.Invoke();
+    }
+}
---
+++ Assets/UGSAuthenticator.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Authentication.PlayerAccounts;
+using Unity.Services.Core;
+using UnityEngine;
+
+public class UGSAuthenticator : MonoBehaviour
+{
+    private async void Start()
+    {
+        try
+        {
+            await UnityServices.InitializeAsync();
+            PlayerAccountService.Instance.SignedIn += SignInWithUnityAuth;
+        }
+        catch (Exception e)
+        {
+            throw; // TODO handle exception
+        }
+    }
+
+    public void SignIn()
+    {
+        StartPlayerAccountsSignInAsync();
+    }
+
+    public void SignInAnonymous()
+    {
+        if (!PlayerAccountService.Instance.IsSignedIn)
+        {
+            SignUpAnonymouslyAsync();
+        }
+        else
+        {
+            Debug.Log(PlayerAccountService.Instance.AccessToken);
+        }
+    }
+    
+    async void StartPlayerAccountsSignInAsync()
+    {
+        if (PlayerAccountService.Instance.IsSignedIn)
+        {
+            // If the player is already signed into Unity Player Accounts, proceed directly to the Unity Authentication sign-in.
+            SignInWithUnityAuth();
+            return;
+        }
+
+        try
+        {
+            // This will open the system browser and prompt the user to sign in to Unity Player Accounts
+            await PlayerAccountService.Instance.StartSignInAsync();
+        }
+        catch (PlayerAccountsException ex) {
+            // Compare error code to PlayerAccountsErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+    
+    async void SignInWithUnityAuth()
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInWithUnityAsync(PlayerAccountService.Instance.AccessToken);
+            Debug.Log("SignIn is successful.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (Exception e)
+        {
+            throw; // TODO handle exception
+        }
+    }
+    
+    async Task LinkWithUnityAsync(string accessToken)
+    {
+        try
+        {
+            await AuthenticationService.Instance.LinkWithUnityAsync(accessToken);
+            Debug.Log("Link is successful.");
+        }
+        catch (AuthenticationException ex) when (ex.ErrorCode == AuthenticationErrorCodes.AccountAlreadyLinked)
+        {
+            // Prompt the player with an error message.
+            Debug.LogError("This user is already linked with another account. Log in instead.");
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+    
+    async Task SignUpAnonymouslyAsync()
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            Debug.Log("Sign in anonymously succeeded!");
+
+            // Shows how to get the playerID
+            Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
+
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+}
