2ac57d0faa63f3262e96bee3f2706c4c17e0844e
AleksisBoy <aleboj9@gmail.com>
2025-05-07 11:45:45 +0200

    Update Example.cs

--- WishHappyNewYear/Project/Example.cs
+++ WishHappyNewYear/Project/Example.cs
@@ -6,40 +6,49 @@ using Unity.Services.CloudCode.Shared;
 using System.Threading.Tasks;
 using System;
 
-namespace WishHappyNewYear;
+namespace LeaderboardsNotification;
 
-public class HappyNewYear
+public class LeaderboardsNotification
 {
-    private readonly ILogger<HappyNewYear> _logger;
+    private readonly ILogger<LeaderboardsNotification> _logger;
 
-    public HappyNewYear(ILogger<HappyNewYear> logger)
+    public LeaderboardsNotification(ILogger<LeaderboardsNotification> logger)
     {
         _logger = logger;
     }
 
-    [CloudCodeFunction("SendProjectMessage")]
-    public async Task SendProjectMessage(IExecutionContext context, PushClient pushClient, string message, string messageType)
+    [CloudCodeFunction("SendPlayerMessage")]
+    public async Task SendPlayerMessage(IExecutionContext context, IGameApiClient gameApiClient, PushClient pushClient, string playerId, string playerName,
+        string leaderboardId, double score)
     {
         try
         {
-            await pushClient.SendProjectMessageAsync(context, message, messageType);
+            var closestPlayers = await gameApiClient.Leaderboards.GetLeaderboardScoresPlayerRangeAsync(context,
+                context.ServiceToken, new Guid(context.ProjectId), leaderboardId, playerId, 1);
+
+            if (closestPlayers.Data.Results.Count != 0)
+            {
+                var player = closestPlayers.Data.Results[^1];
+                string message =
+                    $"The player {playerName} has just beaten your score of {player.Score} on the {leaderboardId} leaderboard by {score - player.Score} points!";
+
+                await pushClient.SendPlayerMessageAsync(context, message, "Information", player.PlayerId);
+            }
         }
         catch (ApiException e)
         {
-            _logger.LogError("Failed to send project message. Error: {Error}", e.Message);
-            throw new Exception($"Failed to send project message. Error: {e.Message}");
+            _logger.LogError("Failed to send push notification to player {playerId}. Error: {Error}", playerId, e.Message);
+            throw new Exception($"Failed to send push notification to player {playerId}. Error: {e.Message}");
         }
     }
-
 }
+
 
 public class ModuleConfig : ICloudCodeSetup
 {
     public void Setup(ICloudCodeConfig config)
     {
         config.Dependencies.AddSingleton(PushClient.Create());
-
+        config.Dependencies.AddSingleton(GameApiClient.Create());
     }
 }
-
-
